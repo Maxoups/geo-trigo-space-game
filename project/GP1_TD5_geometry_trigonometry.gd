97e36ddbbb0b9f4433fcff0838ef2e9749ba57bf
Maxoups <mm.maxencemaire@gmail.com>
2025-10-05 11:26:58 +0200

    added get missile velocity function

--- project/GP1_TD5_geometry_trigonometry.gd
+++ project/GP1_TD5_geometry_trigonometry.gd
@@ -5,8 +5,13 @@ extends Node
 
 
 
-
+# Exercice 2
 func get_satellite_position(orbit_center : Vector2, orbit_duration : float,
 							orbit_start : Vector2, current_time : float) -> Vector2:
 	# Votre code ici
+	return Vector2.ZERO
+
+# Exercice ???
+func get_missile_velocity(position : Vector2, target_position : Vector2, speed : float, 
+							delta : float, current_velocity := Vector2.ZERO) -> Vector2:
 	return Vector2.ZERO
--- project/scripts/missile.gd
+++ project/scripts/missile.gd
@@ -3,20 +3,21 @@ class_name Missile
 
 
 @export var speed := 1000.0
+@export var target_position : Vector2
+@export var velocity : Vector2
 var has_exploded := false
 
 
 func _process(delta: float) -> void:
-	pass
-
-func get_missile_velocity(position : Vector2, target_position : Vector2, speed : float) -> Vector2:
-	return Vector2.ZERO
+	velocity = GP1_TD.get_missile_velocity(position, target_position, speed, delta, velocity)
+	position += velocity
 
 func explode() -> void:
 	if has_exploded:
 		return
 	has_exploded = true
-	
+	ExplosionEffect.spawn_explosion_effect(global_position)
+	queue_free()
 
 func _on_area_collision_body_entered(body: Node2D) -> void:
 	if body.has_method("destroy"):
